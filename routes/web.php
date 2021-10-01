3440265ac3727bcadcb63a018768521477e9d446
Carlos Zapata <cdanielzt@gmail.com>
2021-10-01 14:58:59 -0500

    agregar rutas para colecciones y serializacion

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,63 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 use App\Post;
 
 Route::get('eloquent', function () {
-  $posts = Post::where('id', '>=', '20')
-  ->orderBy('id', 'desc')
-  ->take(3)
-  ->get();
+    $posts = Post::where('id', '>=', '20')
+        ->orderBy('id', 'desc')
+        ->take(3)
+        ->get();
 
-  foreach($posts as $post)
-    {
-    echo "$post->id $post->title <br>";
+    foreach ($posts as $post) {
+        echo "$post->id $post->title <br>";
     }
 });
 
 Route::get('posts', function () {
-  
-  $posts = Post::get();
 
-  foreach($posts as $post){
-  echo "
-  $post->id
-  <strong>{$post->user->name}</strong>
-  $post->title <br>";
-}
+    $posts = Post::get();
+
+    foreach ($posts as $post) {
+        echo "
+        $post->id
+        <strong>{$post->user->name}</strong>
+        $post->title <br>";
+    }
+});
+
+use App\User;
+
+Route::get('users', function () {
+
+    $users = User::all();
+
+    foreach ($users as $user) {
+        echo "
+        $user->id
+        <strong>{$user->name}</strong>
+        {$user->post->count()} <br>";
+    }
+});
+
+Route::get('collections', function () {
+
+    $users = User::all();
+
+    // dd($users->contains(4));
+    // dd($users->except([1,2,3]));
+    // dd($users->only(4));
+    // dd($users->find(4));
+    dd($users->load('post'));
+});
+
+Route::get('serialization', function () {
+
+    $users = User::all();
+
+    $user = $users->find(1);
+
+    // dd($user);
+    dd($user->toJson());
 });
