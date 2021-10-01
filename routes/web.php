554d7b9b9138c4315e5c85d926434099486dfecd
Carlos de leon <carlos.deleon1999@hotmail.com>
2021-10-01 14:24:18 -0500

    agregar rutas y vistas de usuarios

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,42 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 use App\Post;
 
 Route::get('eloquent', function () {
   $posts = Post::where('id', '>=', '20')
-  ->orderBy('id', 'desc')
-  ->take(3)
-  ->get();
+    ->orderBy('id', 'desc')
+    ->take(3)
+    ->get();
 
-  foreach($posts as $post)
-    {
+  foreach ($posts as $post) {
     echo "$post->id $post->title <br>";
-    }
+  }
 });
 
 Route::get('posts', function () {
-  
+
   $posts = Post::get();
 
-  foreach($posts as $post){
-  echo "
+  foreach ($posts as $post) {
+    echo "
   $post->id
   <strong>{$post->user->name}</strong>
   $post->title <br>";
-}
+  }
+});
+
+use App\User;
+
+Route::get('users', function () {
+
+  $users = User::all();
+
+  foreach ($users as $user) {
+    echo "
+  $user->id
+  <strong>{$user->name}</strong>
+  {$user->post->count()} <br>";
+  }
 });
